8e9a0a3c2edc195aa349a0a02162af6511838a38
13878118710 <kir_aster@foxmail.com>
2023-04-17 22:53:49 +0800

    rewrite func

--- lib/filter_by.py
+++ lib/filter_by.py
@@ -9,19 +9,21 @@ from termcolor import colored
 
 # IP地址-筛选
 def filter_by_ip(comtent):
-    while True:
+    ip_list = []
 
-        ip_list = []
-
+    # 对输入的内容以逗号分割进行for循环
+    # 即使输入单个的IP地址或网络或范围，最后也会返回一个单个元素列表
+    for i in comtent.split(','):
+        i = i.strip()
+        # print(i)
         try:
             # 尝试将输入解析为 IPv4Address ,单IP地址处理
-            ip = str(ipaddress.IPv4Address(comtent))
-            return ip
-
+            ip = str(ipaddress.IPv4Address(i))
+            ip_list.append(ip)
         except ValueError:
             try:
                 # 将输入拆分为两个 IP 地址
-                start, end = comtent.split('-')
+                start, end = i.split('-')
                 # 尝试将输入解析为 IPv4Address
                 ipaddress.IPv4Address(start.strip())
                 ipaddress.IPv4Address(end.strip())
@@ -30,22 +32,15 @@ def filter_by_ip(comtent):
                 for ip in range(start_ip, end_ip+1):
                     ip = ipaddress.IPv4Address(ip)
                     ip_list.append(str(ip))
-
-                return ip_list
-
             except ValueError as e:
                 # 尝试将输入解析为 IPv4Network
                 try:
-                    net = ipaddress.IPv4Network(comtent)
+                    net = ipaddress.IPv4Network(i)
                     # IP网段处理
-                    # nr_fitlter = 'IPv4Network'
                     if net.hostmask != '0.0.0.0':
-                        nr_list = []
                         for ip in net.hosts():
                             ip_list.append(str(ip))
-
-                    return ip_list
-
                 except ValueError:
-                    print(colored('不是 IPv4 地址、IPv4 网络或 IPv4 地址范围格式', 'red'))
-                    break
+                    print(colored(f'无法解析IP地址或IP地址范围或IP网络：{i}', 'red'))
+
+    return ip_list
